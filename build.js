7765214e04f0f0603d48da070bbe6919f75f20fb
Yann Bertrand <5855339+yannbertrand@users.noreply.github.com>
2025-09-14 18:51:06 +0200

    ✨ Add index.html dist file

--- build.js
+++ build.js
@@ -24,15 +24,18 @@ const matches = {
 }
 const poules = Object.keys(matches)
 
+console.log(`Build started - ${codent} ${saison}`)
+
 let nbOfMatchesFound = 0
 for (const poule of poules) {
   matches[poule].push(...(await getSaison(saison, codent, poule)))
   nbOfMatchesFound += matches[poule].length
-  console.log(`${matches[poule].length} matches found in ${poule} poule`)
+  console.log(`${matches[poule].length} matches found in poule ${poule}`)
 }
 
 console.log(`${nbOfMatchesFound} matches found`)
 
+let nbOfClubsFound = 0
 const clubMatches = {}
 for (const poule of poules) {
   if (clubMatches[poule] === undefined) {
@@ -42,9 +45,11 @@ for (const poule of poules) {
   for (const match of matches[poule]) {
     if (clubMatches[poule][match.equipeA.nom] === undefined) {
       clubMatches[poule][match.equipeA.nom] = []
+      nbOfClubsFound++
     }
     if (clubMatches[poule][match.equipeB.nom] === undefined) {
       clubMatches[poule][match.equipeB.nom] = []
+      nbOfClubsFound++
     }
     clubMatches[poule][match.equipeA.nom].push(match)
     clubMatches[poule][match.equipeB.nom].push(match)
@@ -52,19 +57,44 @@ for (const poule of poules) {
 }
 
 for (const poule of poules) {
+  clubMatches[poule] = Object.keys(clubMatches[poule])
+    .sort()
+    .reduce((obj, equipe) => {
+      obj[equipe] = clubMatches[poule][equipe]
+      return obj
+    }, {})
+}
+
+console.log(`${nbOfClubsFound} clubs found`)
+
+let htmlFile = `<h1>Agenda volley - ${codent} ${saison}</h1>`
+
+for (const poule of poules) {
+  htmlFile += `<h2>${poule}</h2>`
+  htmlFile += `<ul>`
   for (const club of Object.keys(clubMatches[poule])) {
-    console.log(club)
     const ical = ics(clubMatches[poule][club]).toString()
 
     if (!existsSync('dist')) {
       await mkdir('dist')
     }
-    await writeFile(
-      `dist/${club.replace(' ', '-')}-${codent}-${saison.replace(
-        '/',
-        '-'
-      )}.ics`,
-      ical
-    )
+    const fileName = getFileName(club, codent, saison)
+    await writeFile(`dist/${fileName}`, ical)
+
+    htmlFile += `<li><a href="${fileName}">${fileName}</a></li>`
+    console.log(`Wrote "${fileName}"`)
   }
+  htmlFile += `</ul>`
+}
+
+await writeFile(`dist/index.html`, htmlFile)
+console.log(`Wrote "index.html"`)
+
+console.log(`Build done - ${codent} ${saison}`)
+
+function getFileName(club, codent, saison) {
+  return `${club.replaceAll(' ', '-')}-${codent}-${saison.replace(
+    '/',
+    '-'
+  )}.ics`
 }
